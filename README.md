22864f16fd6c236a036c5d06dbc0d6ef09afe9e2
mohammadsdq <97308791+Mohammadsdq@users.noreply.github.com>
2023-06-17 17:25:23 +0330

    file uploaded

---
+++ README.md
@@ -0,0 +1,28 @@
+# Clock #
+Clock hand
+
+
+## Programming languages used ##
+Input:
+- `HTML`
+- `CSS`
+- `JS`
+
+
+## Features ##
+- Custom colors.
+- Best design
+- modern
+
+
+## Installation ##
+redy to use
+```
+<3  :))
+```
+
+
+## Changelog ##
+#### Version 1 ####
+- Initial release.
+- fixed.
---
+++ main.js
@@ -0,0 +1,20 @@
+const hourHand = document.querySelector(".hour-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const secondHand = document.querySelector(".second-hand");
+
+function rotateTime() {
+  const date = new Date()
+  const h = date.getHours() % 12;
+  const m = date.getMinutes();
+  const s = date.getSeconds();
+
+  const hRotate = (h * 30) + (m / 2);
+  const mRotate = (m * 6) + (s / 10);
+  const sRotate = s * 6;
+
+  hourHand.style.transform = `translateX(-50%) rotate(${hRotate}deg)`;
+  minuteHand.style.transform = `translateX(-50%) rotate(${mRotate}deg)`;
+  secondHand.style.transform = `translateX(-50%) rotate(${sRotate}deg)`;
+};
+
+setInterval(rotateTime, 1000)
